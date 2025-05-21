0ec450f3cc54e7b8fb5ba0f568ef8be3c809ed6f
Raj Mahil <webtest10111011@gmail.com>
2025-05-21 10:30:02 -0600

    update

---  dbtest.php
+++  dbtest.php
@@ -4,4 +4,4 @@ echo "Host: " . getenv('WORDPRESS_DB_HOST') . "\n";
 echo "User: " . getenv('WORDPRESS_DB_USER') . "\n";
 echo "Pass: " . getenv('WORDPRESS_DB_PASSWORD') . "\n";
 echo "DB: "   . getenv('WORDPRESS_DB_NAME') . "\n";
-?>
+?> 
