5ffee4bf6255494940ead427ec10a9957399ee61
Raj Mahil <webtest10111011@gmail.com>
2025-06-02 21:48:43 -0600

    update

--- wp-content/plugins/my-first-block/build/hero-section/index.asset.php
+++ wp-content/plugins/my-first-block/build/hero-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '058c6b14dcd8dc379ad8');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e4725144ae1a1a334487');
