56d9a8f275b4c4f643feb9f9acb2da7cea505a78
Raj Mahil <webtest10111011@gmail.com>
2025-06-03 14:46:31 -0600

    update

--- wp-content/plugins/my-first-block/index.php
+++ wp-content/plugins/my-first-block/index.php
@@ -14,13 +14,6 @@ if (!defined('ABSPATH')) {
 function registerBlocks()
 {
     register_block_type(__DIR__ . '/build/hero-section');
-    register_block_type(__DIR__ . '/build/form-block');
+    // register_block_type(__DIR__ . '/build/form-block');
 }
 add_action('init', 'registerBlocks');
-
-function boilerplate_editor_styles()
-{
-    // Enqueue your main theme Tailwind CSS (or a separate editor-specific CSS if needed)
-    wp_enqueue_style('boilerplate-editor-css', get_template_directory_uri() . '/build/style-index.css', [], '1.0');
-}
-add_action('enqueue_block_editor_assets', 'boilerplate_editor_styles');
