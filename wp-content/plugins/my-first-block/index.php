26fc5b7852c3673e0edd1eaff802ec6ffc60f778
Raj Mahil <webtest10111011@gmail.com>
2025-06-03 13:55:04 -0600

    update

--- wp-content/plugins/my-first-block/index.php
+++ wp-content/plugins/my-first-block/index.php
@@ -17,3 +17,10 @@ function registerBlocks()
     register_block_type(__DIR__ . '/build/form-block');
 }
 add_action('init', 'registerBlocks');
+
+function boilerplate_editor_styles()
+{
+    // Enqueue your main theme Tailwind CSS (or a separate editor-specific CSS if needed)
+    wp_enqueue_style('boilerplate-editor-css', get_template_directory_uri() . '/build/style-index.css', [], '1.0');
+}
+add_action('enqueue_block_editor_assets', 'boilerplate_editor_styles');
