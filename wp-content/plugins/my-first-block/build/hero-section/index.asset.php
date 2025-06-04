b2a1a6a47d2b4d80e65362c2451868d54a8c8359
ayush2t1 <ayush@306technologies.com>
2025-06-04 13:41:46 -0600

    update

--- wp-content/plugins/my-first-block/build/hero-section/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c733da92ccdfdc9f6390');
--- wp-content/plugins/my-first-block/build/showcase-section/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '73b682c9e32ceac7e2ab');
