356361c288e7fe61e9eeb5faf745d11fa1b50cea
Raj Mahil <webtest10111011@gmail.com>
2025-06-04 11:46:08 -0600

    update

--- wp-content/plugins/my-first-block/webpack.config.js
+++ wp-content/plugins/my-first-block/webpack.config.js
@@ -1,4 +1,5 @@
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const path = require( 'path' );
 
 const cleanedDefaultPlugins = defaultConfig.plugins.filter( ( plugin ) => {
 	if ( [ 'RtlCssPlugin' ].includes( plugin.constructor.name ) ) {
@@ -10,6 +11,34 @@ const cleanedDefaultPlugins = defaultConfig.plugins.filter( ( plugin ) => {
 module.exports = {
 	...defaultConfig,
 	plugins: [ ...cleanedDefaultPlugins ],
+	entry: {
+		// Block entry points (JS only - no CSS imports)
+		'hero-section/index': path.resolve(
+			process.cwd(),
+			'src/hero-section',
+			'index.js'
+		),
+		'features-section/index': path.resolve(
+			process.cwd(),
+			'src/features-section',
+			'index.js'
+		),
+		'showcase-section/index': path.resolve(
+			process.cwd(),
+			'src/showcase-section',
+			'index.js'
+		),
+		'form-block/index': path.resolve(
+			process.cwd(),
+			'src/form-block',
+			'index.js'
+		),
+		'form-block/frontend': path.resolve(
+			process.cwd(),
+			'src/form-block',
+			'frontend.js'
+		),
+	},
 	optimization: {
 		...defaultConfig.optimization,
 		splitChunks: {
