67e8efbb60471b5ac909fa70dcf13524f98e8c6c
ayush2t1 <ayush@306technologies.com>
2025-06-04 09:03:09 -0600

    update

--- wp-content/plugins/my-first-block/src/hero-section/index.js
+++ wp-content/plugins/my-first-block/src/hero-section/index.js
@@ -1,6 +1,6 @@
 import { registerBlockType } from '@wordpress/blocks';
 import Edit from './edit';
-import save from './save';
+// import save from './save';
 import metadata from './block.json';
 
 registerBlockType( metadata.name, {
