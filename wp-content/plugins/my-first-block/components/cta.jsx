0d4e49a970db074903279aba0b357c039de3cb56
ayush2t1 <ayush@306technologies.com>
2025-06-05 16:31:04 -0600

    update

--- wp-content/plugins/my-first-block/components/cta.jsx
+++ wp-content/plugins/my-first-block/components/cta.jsx
@@ -8,8 +8,8 @@ const Cta = ( {
 } ) => {
 	return (
 		<section className="not-prose section-padding w-full static-background">
-			<div className="relative max-w-container mx-auto flex items-center justify-between gap-5 bg-brand-dark-blue text-white py-16 px-10 rounded-xl w-full">
-				<div className="flex flex-col gap-4 w-full max-w-[60%]">
+			<div className="relative max-w-container mx-auto  bg-brand-dark-blue text-white py-16 px-10 rounded-xl w-full">
+				<div className="flex flex-col gap-4 w-full  md:max-w-[70%] lg:max-w-[60%]">
 					<div className="flex flex-col gap-3 w-full">
 						<p className="decorative-text !text-brand-yellow-dark text-3xl lg:!text-4xl !my-0 text-center sm:text-left">
 							{ topHeading }
@@ -24,23 +24,23 @@ const Cta = ( {
 						</button>
 					</a>
 				</div>
-				<div className="absolute right-10 w-full flex items-center gap-5 justify-end max-w-[40%] bottom-14">
+				<div className="absolute right-1/4 md:right-10 w-full flex items-center gap-5 justify-end max-w-[60%] md:max-w-[40%] bottom-[300px] md:bottom-14">
 					<div className="relative">
 						<div className="flex justify-center items-center relative ">
 							{ image?.url && (
-								<div className="w-full aspect-[3/4] max-w-[200px] sm:max-w-[260px] md:max-w-[340px] rounded-lg shadow-lg rotate-[-8deg]  bg-white p-1">
+								<div className="w-full aspect-[3/4] max-w-[260px] md:max-w-[340px] rounded-lg shadow-lg rotate-[-8deg]  bg-white p-1">
 									<img
 										src={ image.url }
-										className="w-full h-full aspect-[3/4] max-w-[200px] sm:max-w-[260px] md:max-w-[340px] object-cover "
+										className="w-full h-full aspect-[3/4] max-w-[260px] md:max-w-[340px] object-cover "
 									/>
 								</div>
 							) }
 
 							{ image2?.url && (
-								<div className="w-full  aspect-[3/4] max-w-[200px] sm:max-w-[260px] md:max-w-[340px]  rounded-lg shadow-lg  -ml-24 sm:-ml-32 lg:-ml-[160px] z-10 bg-white p-1 rotate-[2deg]">
+								<div className="w-full  aspect-[3/4] max-w-[260px] md:max-w-[340px]  rounded-lg shadow-lg  -ml-24 sm:-ml-32 lg:-ml-[160px] z-10 bg-white p-1 rotate-[2deg]">
 									<img
 										src={ image2.url }
-										className="w-full h-full aspect-[3/4] max-w-[200px] sm:max-w-[260px] md:max-w-[340px] object-cover "
+										className="w-full h-full aspect-[3/4] max-w-[260px] md:max-w-[340px] object-cover "
 									/>
 								</div>
 							) }
