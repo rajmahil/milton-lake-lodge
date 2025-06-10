060183ea864a925d6f7d0035349d04b69d697561
ayush2t1 <ayush@306technologies.com>
2025-06-10 17:00:28 -0600

    update

--- wp-content/plugins/my-first-block/src/gallery-section/render.php
+++ wp-content/plugins/my-first-block/src/gallery-section/render.php
@@ -203,13 +203,13 @@ $totalImages = count($images);
                                 alt=""
                             >
                             
-                            <div class="absolute -bottom-2 left-1/2 transform -translate-x-1/2 bg-black/90 text-white py-1 px-4 rounded-full text-sm font-medium xl:block hidden">
+                            <div class="absolute -bottom-2 left-1/2 transform -translate-x-1/2 bg-black/90 text-white py-1 px-4 rounded-full text-sm font-medium lg:block !hidden">
                                 <span x-text="imageGalleryImageIndex + 1"></span>
                                 <span>/</span>
                                 <span x-text="totalImages"></span>
                             </div>
 
-                            <div class="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex items-center gap-4 xl:hidden">
+                            <div class="absolute -bottom-4 left-1/2 transform -translate-x-1/2 flex items-center gap-4 lg:!hidden">
                                 <div 
                                     @click="$event.stopPropagation(); imageGalleryPrev()" 
                                     class="flex items-center justify-center text-white rounded-full cursor-pointer bg-white/10 w-10 sm:w-14 h-10 m:h-14  hover:bg-white/20 transition-colors duration-200"
@@ -247,7 +247,7 @@ $totalImages = count($images);
 
                         <div 
                             @click="imageGalleryClose" 
-                            class="absolute top-4 right-4 flex items-center justify-center text-white bg-white/10 w-12 h-12 rounded-full cursor-pointer hover:bg-white/20 transition-colors duration-200"
+                            class="fixed top-10 right-10 flex items-center justify-center text-white bg-white/10 w-12 h-12 rounded-full cursor-pointer hover:bg-white/20 transition-colors duration-200"
                         >
                             <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
