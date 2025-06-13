9a8bf82c6add462b3cbb894e7c2679a6055ac4ba
Raj Mahil <webtest10111011@gmail.com>
2025-06-13 10:10:40 -0600

    update

--- wp-config.php
+++ wp-config.php
@@ -23,20 +23,6 @@
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 // ** Database settings from environment ** //
-define('WP_REDIS_SCHEME', 'tcp');
-define('WP_REDIS_HOST', 'crossover.proxy.rlwy.net');
-define('WP_REDIS_PORT', 40516);
-define('WP_REDIS_CLIENT', 'predis');
-define('WP_REDIS_USERNAME', 'default');
-define('WP_REDIS_PASSWORD', 'LctkHNPFReQWgebFsfiuieehYcqZvtZl');
-define('WP_REDIS_PREFIX', 'miltonlake_');
-define('WP_REDIS_DATABASE', 0);
-define('WP_REDIS_TIMEOUT', 1);
-define('WP_REDIS_READ_TIMEOUT', 1);
-define('WP_REDIS_SSL_CONTEXT', [
-    'verify_peer' => false,
-    'verify_peer_name' => false,
-]);
 
 //exchange rate api
 define('EXCHANGE_RATE_API_KEY', '7128e4810a5d0bca289f546c');
--- wp-content/themes/theme-tailwind/functions.php
+++ wp-content/themes/theme-tailwind/functions.php
@@ -359,3 +359,32 @@ add_filter('acf/load_field/name=trip_type', function ($field) {
     }
     return $field;
 });
+
+add_action('after_setup_theme', function () {
+    // Only define if not already defined (to avoid conflicts)
+    if (!defined('WP_REDIS_HOST')) {
+        define('WP_REDIS_SCHEME', 'tcp');
+        define('WP_REDIS_HOST', 'crossover.proxy.rlwy.net');
+        define('WP_REDIS_PORT', 40516);
+        define('WP_REDIS_CLIENT', 'predis');
+        define('WP_REDIS_USERNAME', 'default');
+        define('WP_REDIS_PASSWORD', 'LctkHNPFReQWgebFsfiuieehYcqZvtZl');
+        define('WP_REDIS_PREFIX', 'miltonlake_');
+        define('WP_REDIS_DATABASE', 0);
+        define('WP_REDIS_TIMEOUT', 1);
+        define('WP_REDIS_READ_TIMEOUT', 1);
+        define('WP_REDIS_SSL_CONTEXT', [
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ]);
+
+        // Debug log to confirm these are being set
+        error_log('Redis constants defined in functions.php');
+        error_log('WP_REDIS_HOST: ' . WP_REDIS_HOST);
+    }
+
+    if (!defined('EXCHANGE_RATE_API_KEY')) {
+        define('EXCHANGE_RATE_API_KEY', '7128e4810a5d0bca289f546c');
+        error_log('EXCHANGE_RATE_API_KEY defined in functions.php');
+    }
+});
